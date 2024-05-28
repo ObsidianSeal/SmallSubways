b2772ad15071a0b329e66d0f912f1a325a12a771
ObsidianSeal <bmgosselin@gmail.com>
2024-05-28 13:04:07 -0400

    Station object and Shape enum

---
+++ src/main/Shape.java
@@ -0,0 +1,14 @@
+package main;
+
+public enum Shape {
+    CIRCLE,
+    TRIANGLE,
+    SQUARE,
+    STAR,
+    PENTAGON,
+    GEM,
+    CROSS,
+    WEDGE,
+    DIAMOND,
+    OVAL
+}
---
+++ src/main/Station.java
@@ -0,0 +1,76 @@
+/*
+ * TITLE: Station
+ * AUTHOR: Benjamin Gosselin
+ * DATE: Tuesday, May 28th, 2024
+ * DESCRIPTION: Object; represents a subway station.
+ */
+
+package main;
+
+/**
+ * The Station object.
+ */
+public class Station {
+    private int x, y;
+    private Shape type;
+
+    /**
+     * Station constructor.
+     * @param x Assign the station's x-coordinate.
+     * @param y Assign the station's y-coordinate.
+     * @param type Assign the station's type.
+     */
+    public Station(int x, int y, Shape type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    /**
+     * Get the station's x-coordinate.
+     * @return The station's current x-coordinate.
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Set the station's x-coordinate.
+     * @param x The station's new x-coordinate.
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Get the station's y-coordinate.
+     * @return The station's current y-coordinate.
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Set the station's y-coordinate.
+     * @param y The station's new y-coordinate.
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Get the station's type.
+     * @return The station's current type.
+     */
+    public Shape getType() {
+        return type;
+    }
+
+    /**
+     * Set the station's type.
+     * @param type The station's new type.
+     */
+    public void setType(Shape type) {
+        this.type = type;
+    }
+}
