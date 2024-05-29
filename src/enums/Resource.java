7a055e80c7dfcfabe74afaaf404c1eb6e4c9d28e
ObsidianSeal <bmgosselin@gmail.com>
2024-05-28 20:52:02 -0400

    Resource converted to enum, pressed enter in Tunnel
    I think this is best since the there is an inventory class that will hold these items and their use would be separate anyway

---
+++ src/enums/Resource.java
@@ -0,0 +1,19 @@
+/*
+ * TITLE: Resource
+ * AUTHORS: Daniel Zhong, Benjamin Gosselin
+ * DATE: Tuesday, May 28th, 2024
+ * DESCRIPTION: All the resources that could be held in the player's inventory.
+ */
+
+package enums;
+
+/**
+ * Inventory item representations.
+ */
+public enum Resource {
+    LOCOMOTIVE,
+    CARRIAGE,
+    INTERCHANGE,
+    LINE,
+    TUNNEL
+}
--- src/objects/Tunnel.java
+++ src/objects/Tunnel.java
@@ -11,4 +11,5 @@ package objects;
  *
  */
 public class Tunnel {
+
 }
