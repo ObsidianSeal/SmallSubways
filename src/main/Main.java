4123e6383b289469ae5ba5a079bc593b1c5b710c
ObsidianSeal <bmgosselin@gmail.com>
2024-06-13 09:48:10 -0400

    cleaned all highlighting, reverted double click because we decided against that

--- src/main/Main.java
+++ src/main/Main.java
@@ -73,9 +73,8 @@ public class Main {
     static int mouseX, mouseY;
     static int gridX, gridY;
     static boolean controlHeld = false;
+    static boolean dHeld = false;
     static boolean sHeld = false;
-    static boolean doubleClick = false;
-    static int lastClickedTick = 0;
     static int currentLine;
     static int circleHover = -1;
     public static int[] resources = new int[4];
@@ -370,14 +369,7 @@ public class Main {
                 for (Station station : stations) {
                     if (station.isSelected()) {
                         if (circleHover >= 0) station.highlight(lines[circleHover].getColour());
-                        else {
-                            station.highlight(lines[currentLine].getColour());
-                            for (MetroLine l : lines) {
-                                if (!(l == null)) {
-                                    if (l.getStations().contains(station)) station.highlight(l.getColour());
-                                }
-                            }
-                        }
+                        else station.highlight(lines[currentLine].getColour());
                     }
                     station.draw();
                 }
@@ -574,13 +566,6 @@ public class Main {
                     // back arrow
                     if (gridX >= 3 && gridX < 7 && gridY >= 2 && gridY < 4) screenState = Screen.MAIN_MENU;
                 } else if (screenState == Screen.GAME) {
-                    if (ticks - lastClickedTick < 50) {
-                        doubleClick = true;
-                    } else {
-                        doubleClick = false;
-                    }
-
-                    lastClickedTick = ticks;
                     // back arrow
                     if (gridX >= 3 && gridX < 7 && gridY >= 2 && gridY < 4) screenState = Screen.LEVEL_SELECT;
 
@@ -591,21 +576,17 @@ public class Main {
                             // station is not already on the line
                             if (!lines[currentLine].getStations().contains(station)) {
                                 // if adding to the beginning, set diagonal of FIRST station (which will become the NEXT station)
-                                if (sHeld && !lines[currentLine].getStations().isEmpty()) lines[currentLine].getStations().getFirst().setDiagonal(lines[currentLine], !doubleClick);
-                                else station.setDiagonal(lines[currentLine], doubleClick); // otherwise current station
+                                if (sHeld && !lines[currentLine].getStations().isEmpty()) lines[currentLine].getStations().getFirst().setDiagonal(lines[currentLine], !dHeld);
+                                else station.setDiagonal(lines[currentLine], dHeld); // otherwise current station
 
                                 lines[currentLine].addStation(station, sHeld); // add the station
                             } else {
-                                if (!doubleClick) {
-                                    // you can always remove stations if doing so would make the line invisible
-                                    if (lines[currentLine].getStations().size() <= 2)
-                                        lines[currentLine].removeStation(station);
+                                // you can always remove stations if doing so would make the line invisible
+                                if (lines[currentLine].getStations().size() <= 2) lines[currentLine].removeStation(station);
 
-                                    // make sure the station is not being used by a train!
-                                    for (Train train : lines[currentLine].getTrains()) {
-                                        if (!(station == train.getFromStation() || station == train.getToStation()))
-                                            lines[currentLine].removeStation(station);
-                                    }
+                                // make sure the station is not being used by a train!
+                                for (Train train : lines[currentLine].getTrains()) {
+                                    if (!(station == train.getFromStation() || station == train.getToStation())) lines[currentLine].removeStation(station);
                                 }
                             }
                         }
@@ -695,9 +676,10 @@ public class Main {
 
             if (screenState == Screen.GAME) {
                 // hover
+                circleHover = -1;
+
                 if (gridX >= 54 && gridY >= 38) {
                     // line circle hover
-                    circleHover = -1;
                     for (int i = 0; i < 7; i++) {
                         for (int j = 0; j < 2; j++) {
                             for (int k = 0; k < 2; k++) {
@@ -708,46 +690,18 @@ public class Main {
                             }
                         }
                     }
-                    for (Station station : stations) {
-                        station.setSelected(false);
-                    }
-                    for (int i = 0; i < 7; i++) {
-                        if (i == circleHover) {
-                            for (Station station : lines[i].getStations()) {
-                                station.setSelected(true);
-                            }
-                        }
-                    }
-                } else { // station hover
-                    Station hoveredStation = null;
-
-                    // Finds station being hovered over
+                } else {
+                    // station hover, entire line
                     for (Station station : stations) {
                         if (station.getGridX() == gridX && station.getGridY() == gridY) {
-                            hoveredStation = station;
-                            hoveredStation.setSelected(true);
+                            circleHover = currentLine;
                             break;
                         }
                     }
-
-                    // Unselects stations
-                    if (hoveredStation == null) {
-                        for (Station s : stations) {
-                            s.setSelected(false);
-                        }
-                    }
-
-                    // Finds all lines connected to hovered station
-                    for (MetroLine l : lines) {
-                        if (!(l == null)) {
-                            if (l.getStations().contains(hoveredStation)) {
-                                for (Station s : l.getStations()) {
-                                    s.setSelected(true);
-                                }
-                            }
-                        }
-                    }
                 }
+
+                for (Station station : stations) station.setSelected(station.getGridX() == gridX && station.getGridY() == gridY);
+                if (circleHover >= 0) for (Station station : lines[circleHover].getStations()) station.setSelected(true);
             }
         }
 
@@ -766,6 +720,7 @@ public class Main {
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
                 case (KeyEvent.VK_CONTROL) -> controlHeld = true;
+                case (KeyEvent.VK_D) -> dHeld = true;
                 case (KeyEvent.VK_S) -> sHeld = true;
             }
 
@@ -825,6 +780,7 @@ public class Main {
         public void keyReleased(KeyEvent e) {
             switch (e.getKeyCode()) {
                 case (KeyEvent.VK_CONTROL) -> controlHeld = false;
+                case (KeyEvent.VK_D) -> dHeld = false;
                 case (KeyEvent.VK_S) -> sHeld = false;
             }
         }
